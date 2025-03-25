233ac76cac821e320721fd15156f7da3bfde9886
Kontanten <free-kontanten-dev@ohoy.link>
2025-03-25 08:20:37 +0000

    ui/beta,flask: remove tracking from footer

--- ui/pages/home/beta/beta-home-footer.component.js
+++ ui/pages/home/beta/beta-home-footer.component.js
@@ -1,44 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import {
-  SUPPORT_LINK,
-  SUPPORT_REQUEST_LINK,
-} from '../../../helpers/constants/common';
+import { SUPPORT_LINK } from '../../../helpers/constants/common';
 import { useI18nContext } from '../../../hooks/useI18nContext';
-import {
-  MetaMetricsContextProp,
-  MetaMetricsEventCategory,
-  MetaMetricsEventName,
-} from '../../../../shared/constants/metametrics';
-import { MetaMetricsContext } from '../../../contexts/metametrics';
 
 const BetaHomeFooter = () => {
   const t = useI18nContext();
-  const trackEvent = useContext(MetaMetricsContext);
 
   return (
     <>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={SUPPORT_LINK}
-        onClick={() => {
-          trackEvent(
-            {
-              category: MetaMetricsEventCategory.Footer,
-              event: MetaMetricsEventName.SupportLinkClicked,
-              properties: {
-                url: SUPPORT_REQUEST_LINK,
-              },
-            },
-            {
-              contextPropsIntoEventProperties: [
-                MetaMetricsContextProp.PageTitle,
-              ],
-            },
-          );
-        }}
-      >
+      <a target="_blank" rel="noopener noreferrer" href={SUPPORT_LINK}>
         {t('needHelpSubmitTicket')}
       </a>
     </>
--- ui/pages/home/flask/flask-home-footer.component.js
+++ ui/pages/home/flask/flask-home-footer.component.js
@@ -1,41 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import { SUPPORT_REQUEST_LINK } from '../../../helpers/constants/common';
 import { useI18nContext } from '../../../hooks/useI18nContext';
-import {
-  MetaMetricsContextProp,
-  MetaMetricsEventCategory,
-  MetaMetricsEventName,
-} from '../../../../shared/constants/metametrics';
-import { MetaMetricsContext } from '../../../contexts/metametrics';
 
 const FlaskHomeFooter = () => {
   const t = useI18nContext();
-  const trackEvent = useContext(MetaMetricsContext);
 
   return (
     <>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={SUPPORT_REQUEST_LINK}
-        onClick={() => {
-          trackEvent(
-            {
-              category: MetaMetricsEventCategory.Footer,
-              event: MetaMetricsEventName.SupportLinkClicked,
-              properties: {
-                url: SUPPORT_REQUEST_LINK,
-              },
-            },
-            {
-              contextPropsIntoEventProperties: [
-                MetaMetricsContextProp.PageTitle,
-              ],
-            },
-          );
-        }}
-      >
+      <a target="_blank" rel="noopener noreferrer" href={SUPPORT_REQUEST_LINK}>
         {t('needHelpSubmitTicket')}
       </a>{' '}
       |{' '}
