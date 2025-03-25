2ee908abd2eecebc6184bc937dbc70c1c2bb1299
Kontanten <free-kontanten-dev@ohoy.link>
2025-03-25 08:20:55 +0000

    ui/notifications: remove tracking

--- ui/pages/notifications-settings/notifications-settings-per-account.tsx
+++ ui/pages/notifications-settings/notifications-settings-per-account.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useContext } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useSwitchAccountNotificationsChange } from '../../hooks/metamask-notifications/useSwitchNotifications';
 import {
   NotificationsSettingsBox,
