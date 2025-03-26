bc5fed0388ebdcaf7276d88848357d0341bfac75
Kontanten <free-kontanten-dev@ohoy.link>
2025-03-26 00:05:52 +0000

    ui: remove promotional banner Carousel from account-overview

--- ui/components/multichain/account-overview/account-overview-layout.tsx
+++ ui/components/multichain/account-overview/account-overview-layout.tsx
@@ -1,19 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getSelectedAccountCachedBalance } from '../../../selectors';
+import React from 'react';
 import {
   AccountOverviewTabsProps,
   AccountOverviewTabs,
 } from './account-overview-tabs';
-import {
-  FUND_SLIDE,
-  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-  BRIDGE_SLIDE,
-  ///: END:ONLY_INCLUDE_IF
-  CARD_SLIDE,
-  CASH_SLIDE,
-  ZERO_BALANCE,
-} from './constants';
 
 export type AccountOverviewLayoutProps = AccountOverviewTabsProps & {
   children: React.ReactElement;
@@ -23,34 +12,6 @@ export const AccountOverviewLayout = ({
   children,
   ...tabsProps
 }: AccountOverviewLayoutProps) => {
-  const dispatch = useDispatch();
-  const totalBalance = useSelector(getSelectedAccountCachedBalance);
-
-  const hasZeroBalance = totalBalance === ZERO_BALANCE;
-
-  useEffect(() => {
-    const fundSlide = {
-      ...FUND_SLIDE,
-      undismissable: hasZeroBalance,
-    };
-
-    const defaultSlides = [
-      ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-      BRIDGE_SLIDE,
-      ///: END:ONLY_INCLUDE_IF
-      CARD_SLIDE,
-      CASH_SLIDE,
-    ];
-
-    if (hasZeroBalance) {
-      defaultSlides.unshift(fundSlide);
-    } else {
-      defaultSlides.splice(2, 0, fundSlide);
-    }
-
-    dispatch(updateSlides(defaultSlides));
-  }, [hasZeroBalance]);
-
   return (
     <>
       <div className="account-overview__balance-wrapper">{children}</div>
