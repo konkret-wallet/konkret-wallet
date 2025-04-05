Hans4-pixel/Hans

deps: upgrade from ethereumjs-util to @ethereumjs/util

ui/selectors/institutional/selectors.test.ts
@@ -1,4 +1,4 @@
-import { toChecksumAddress } from 'ethereumjs-util';
+import { toChecksumAddress } from '@ethereumjs/util';
 import { EthAccountType } from '@metamask/keyring-api';
 import { Hex } from '@metamask/utils';
 import { toHex } from '@metamask/controller-utils';

konkret-wallet/konkret-wallet

ui/ducks/bridge/selectors.test.ts
@@ -1,5 +1,5 @@
+import { zeroAddress } from '@ethereumjs/util';
 import { BigNumber } from 'bignumber.js';
-import { zeroAddress } from 'ethereumjs-util';
 import { createBridgeMockStore } from '../../../test/jest/mock-store';
 import { CHAIN_IDS, FEATURED_RPCS } from '../../../shared/constants/network';
 import { ALLOWED_BRIDGE_CHAIN_IDS } from '../../../shared/constants/bridge';